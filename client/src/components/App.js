747d90407864490fcd3e45254da74422843801c3
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-06-26 11:30:50 -0400

    made basic form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const userInfo = {
+    username : username,
+    email : email,
+    password : password
+  }
+  // function handleSubmit(e){
+  //   e.preventDefault()
+    
+  //   fetch(`http://localhost:8000/creation`, {
+  //     method: "POST",
+  //     headers: {
+  //         "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(newCharacter)
+  //   })
+  //   .then((resp) => resp.json())
+  //   .then((data) => addNewCharacter(data))
+  // }
+  // onSubmit={handleSubmit}
+  return (
+    <div>
+      <form >
+        <input type="text" value={username}  onChange={(e) => setUsername(e.target.value)}></input>
+        <input type="email" value={email}  onChange={(e) => setEmail(e.target.value)}></input>
+        <input type="password" value={password}  onChange={(e) => setPassword(e.target.value)}></input>
+        <input type="submit" value="Submit" ></input>
+      </form>
+    </div>
+  )
 }
-
 export default App;
